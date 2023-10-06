3dd73089084aec17bc66b50f90cbfa2344bb8dce
JUNGI JEONG <junejjk@gmail.com>
2023-10-06 14:17:08 +0900

    2nd

--- src/network/api.jsx
+++ src/network/api.jsx
@@ -1,24 +1,25 @@
-import axios from "axios"
+import axios from "axios";
 axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development' ?
-        'http://localhost:8090' : 'http://34.122.162.50:8090';
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8090"
+    : "http://35.238.13.75:8090";
 
 export const apiNoToken = async (url, method, data) => {
-    const body = await axios(
-        {
-            url, method, data
-        }
-    )
-    return body
-}
+  const body = await axios({
+    url,
+    method,
+    data,
+  });
+  return body;
+};
 export const api = async (url, method, data) => {
-    const token = localStorage.getItem('token')
-    const body = await axios(
-        {
-            url, method, data, headers: { "Authorization": `Bearer ${token}` }
-        }
-    )
+  const token = localStorage.getItem("token");
+  const body = await axios({
+    url,
+    method,
+    data,
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
-    return body
-
-}
+  return body;
+};
